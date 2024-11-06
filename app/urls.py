9f7af2b6e82cef6e6703590074a0cab4d4fcc026
levi <soromtolevi1@gmail.com>
2024-11-06 07:36:30 +0100

    Added a booking functionality

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ app_name = 'app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('book/', views.book, name='book'),
 ]
---
+++ app/utils.py
@@ -0,0 +1,29 @@
+from typing import Tuple, Dict
+
+
+# Mail
+def send_gmail(*mail: Tuple) -> str:
+    '''
+    This functiion 5 arguments and they are passed accordinly.
+    '''
+    subject, message_body, user_email, user_name, phone = mail
+
+     # Construct the email message
+    message = f"Hello Admin,\n\nYou've received a new message from the contact form on your website.\n\n"
+    message += f"Name: {user_name}\nEmail: {user_email}\nPhone: {phone}\nSubject: {subject}\n\nMessage:\n{message_body}\n\n"
+    message += "Please reply directly to this email to contact the user."
+
+    return message
+
+def book(*mail: Tuple) -> str:
+    '''
+    This functiion 5 arguments and they are passed accordinly.
+    '''
+    subject, message_body, user_email, user_name, date = mail
+
+     # Construct the email message
+    message = f"Hello Admin,\n\nYou've received a new message from the contact form on your website.\n\n"
+    message += f"Name: {user_name}\nEmail: {user_email}\nPhone: {date}\nSubject: {subject}\n\nMessage:\n{message_body}\n\n"
+    message += "Please reply directly to this email to contact the user."
+
+    return message
